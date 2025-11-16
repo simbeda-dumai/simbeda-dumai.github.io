8f0232718f748e67516ddf2c2a319ee9e836e7c1
dumaikota <idamanbpbddumai@gmail.com>
2025-11-17 01:46:45 +0700

    sssssss

--- JS/header.js
+++ JS/header.js
@@ -7,13 +7,13 @@
   function checkAuth() {
     const session = JSON.parse(localStorage.getItem('simbeda_auth'));
     if (session && session.username) {
-      // Tampilkan nama pengguna dan tombol logout
-      $('#login-button').style.display = 'none';  // Sembunyikan tombol login
-      $('#logout-button').style.display = 'inline';  // Tampilkan tombol logout
-      $('#user-name').style.display = 'inline';  // Tampilkan nama pengguna
-      $('#username-display').textContent = session.username;  // Tampilkan nama pengguna
+      // Sembunyikan tombol login, tampilkan tombol logout dan nama pengguna
+      $('#login-button').style.display = 'none';
+      $('#logout-button').style.display = 'inline';
+      $('#user-name').style.display = 'inline';
+      $('#username-display').textContent = session.username;
     } else {
-      // Tampilkan tombol login dan sembunyikan nama pengguna
+      // Tampilkan tombol login, sembunyikan tombol logout
       $('#login-button').style.display = 'inline';
       $('#logout-button').style.display = 'none';
       $('#user-name').style.display = 'none';
@@ -23,7 +23,7 @@
   // Logout dan hapus session
   $('#logout-button').addEventListener('click', function (e) {
     e.preventDefault();
-    localStorage.removeItem('simbeda_auth');  // Hapus session dari localStorage
+    localStorage.removeItem('simbeda_auth');
     checkAuth();  // Perbarui navbar setelah logout
     window.location.href = '/HTML/login.html';  // Arahkan ke halaman login
   });
